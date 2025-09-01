c91e9f1937643fa16a35738d7ebfc0de54603d72
evikhon <159645060+Exalm@users.noreply.github.com>
2025-09-01 20:06:11 +0300

    Update addTrack.js

--- admin/addTrack.js
+++ admin/addTrack.js
@@ -24,10 +24,10 @@ function createSongElement(song) {
       </div>
     </div>
     <div class="music__add right">
-      <div class="dwn" style="cursor:pointer;">
-        <svg fill="#000000" width="24px" height="24px" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><path d="M256,409.7,152.05,305.75,173.5,284.3l67.33,67.32V34h30.34V351.62L338.5,284.3,360,305.75ZM445.92,351v93.22a3.61,3.61,0,0,1-3.47,3.48H69.15a3.3,3.3,0,0,1-3.07-3.48V351H35.74v93.22A33.66,33.66,0,0,0,69.15,478h373.3a33.85,33.85,0,0,0,33.81-33.82V351Z"/></svg>
-      </div>
       <div class="time">${song.time}</div>
+      <div class="dwn" style="cursor:pointer;">
+        <svg fill="#000000" width="28px" height="28px" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><path d="M256,409.7,152.05,305.75,173.5,284.3l67.33,67.32V34h30.34V351.62L338.5,284.3,360,305.75ZM445.92,351v93.22a3.61,3.61,0,0,1-3.47,3.48H69.15a3.3,3.3,0,0,1-3.07-3.48V351H35.74v93.22A33.66,33.66,0,0,0,69.15,478h373.3a33.85,33.85,0,0,0,33.81-33.82V351Z"/></svg>
+      </div>
     </div>
   `;
   return div;
@@ -93,7 +93,7 @@ musicBox.addEventListener('click', (e) => {
     button.style.opacity = '0';
 
     setTimeout(() => {
-      button.innerHTML = '<img src="./images/download_animation.gif" alt="loading animation" style="width:24px; height:24px;">';
+      button.innerHTML = '<img src="./images/download_animation.gif" alt="loading animation" style="width:28px; height:28px;">';
       button.style.opacity = '1';
 
       setTimeout(() => {
@@ -101,7 +101,7 @@ musicBox.addEventListener('click', (e) => {
         setTimeout(() => {
           button.innerHTML = `
             <?xml version="1.0" ?>
-                        <svg fill="#000000" width="24px" height="24px" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><path d="M256,409.7,152.05,305.75,173.5,284.3l67.33,67.32V34h30.34V351.62L338.5,284.3,360,305.75ZM445.92,351v93.22a3.61,3.61,0,0,1-3.47,3.48H69.15a3.3,3.3,0,0,1-3.07-3.48V351H35.74v93.22A33.66,33.66,0,0,0,69.15,478h373.3a33.85,33.85,0,0,0,33.81-33.82V351Z"/></svg>`;
+                        <svg fill="#000000" width="28px" height="28px" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><path d="M256,409.7,152.05,305.75,173.5,284.3l67.33,67.32V34h30.34V351.62L338.5,284.3,360,305.75ZM445.92,351v93.22a3.61,3.61,0,0,1-3.47,3.48H69.15a3.3,3.3,0,0,1-3.07-3.48V351H35.74v93.22A33.66,33.66,0,0,0,69.15,478h373.3a33.85,33.85,0,0,0,33.81-33.82V351Z"/></svg>`;
           button.style.opacity = '1';
         }, 500);
       }, 3000);
