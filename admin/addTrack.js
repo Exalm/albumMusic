e840f3ca03593805db12ae784b93398eefaa2880
evikhon <159645060+Exalm@users.noreply.github.com>
2025-09-01 20:34:22 +0300

    Update addTrack.js

--- admin/addTrack.js
+++ admin/addTrack.js
@@ -26,7 +26,15 @@ function createSongElement(song) {
     <div class="music__add right">
       <div class="time">${song.time}</div>
       <div class="dwn" style="cursor:pointer;">
-        <svg fill="#000000" width="28px" height="28px" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><path d="M256,409.7,152.05,305.75,173.5,284.3l67.33,67.32V34h30.34V351.62L338.5,284.3,360,305.75ZM445.92,351v93.22a3.61,3.61,0,0,1-3.47,3.48H69.15a3.3,3.3,0,0,1-3.07-3.48V351H35.74v93.22A33.66,33.66,0,0,0,69.15,478h373.3a33.85,33.85,0,0,0,33.81-33.82V351Z"/></svg>
+        <?xml version="1.0" ?>
+                        <svg fill="#000000" width="28px" height="28px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+  <path 
+    d="M256,409.7,152.05,305.75,173.5,284.3l67.33,67.32V34h30.34V351.62L338.5,284.3,360,305.75ZM445.92,351v93.22a3.61,3.61,0,0,1-3.47,3.48H69.15a3.3,3.3,0,0,1-3.07-3.48V351H35.74v93.22A33.66,33.66,0,0,0,69.15,478h373.3a33.85,33.85,0,0,0,33.81-33.82V351Z"
+    stroke="#000000"
+    stroke-width="45"
+    stroke-linejoin="round"
+  />
+</svg>
       </div>
     </div>
   `;
@@ -101,7 +109,14 @@ musicBox.addEventListener('click', (e) => {
         setTimeout(() => {
           button.innerHTML = `
             <?xml version="1.0" ?>
-                        <svg fill="#000000" width="28px" height="28px" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><path d="M256,409.7,152.05,305.75,173.5,284.3l67.33,67.32V34h30.34V351.62L338.5,284.3,360,305.75ZM445.92,351v93.22a3.61,3.61,0,0,1-3.47,3.48H69.15a3.3,3.3,0,0,1-3.07-3.48V351H35.74v93.22A33.66,33.66,0,0,0,69.15,478h373.3a33.85,33.85,0,0,0,33.81-33.82V351Z"/></svg>`;
+                        <svg fill="#000000" width="28px" height="28px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+  <path 
+    d="M256,409.7,152.05,305.75,173.5,284.3l67.33,67.32V34h30.34V351.62L338.5,284.3,360,305.75ZM445.92,351v93.22a3.61,3.61,0,0,1-3.47,3.48H69.15a3.3,3.3,0,0,1-3.07-3.48V351H35.74v93.22A33.66,33.66,0,0,0,69.15,478h373.3a33.85,33.85,0,0,0,33.81-33.82V351Z"
+    stroke="#000000"
+    stroke-width="45"
+    stroke-linejoin="round"
+  />
+</svg>`;
           button.style.opacity = '1';
         }, 500);
       }, 3000);
