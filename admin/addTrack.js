2eaf5ed4d5b26e7e429fc51f121861824889f76c
evikhon <159645060+Exalm@users.noreply.github.com>
2025-09-02 11:23:00 +0300

    Update addTrack.js
    устранение бага с проигрывателем

--- admin/addTrack.js
+++ admin/addTrack.js
@@ -1,5 +1,20 @@
 const CURRENT_STORAGE_VERSION = '1';
 
+function isLocalStorageAvailable() {
+  try {
+    const testKey = '__test__';
+    localStorage.setItem(testKey, testKey);
+    localStorage.removeItem(testKey);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+if (!isLocalStorageAvailable()) {
+  alert('Ваш браузер не поддерживает localStorage или он отключен');
+}
+
 // Очистка localStorage при изменении версии
 const savedVersion = localStorage.getItem('storage_version');
 if (savedVersion !== CURRENT_STORAGE_VERSION) {
@@ -18,7 +33,7 @@ function createSongElement(song) {
   div.className = 'music__container';
   div.innerHTML = `
     <div class="left">
-      <div class="openAdminModal player" tabindex="0">&#9658;</div>
+      <div class="openAdminModal player" tabindex="0"><img src="./images/player.svg" width="28px" height="28px" class="openAdminModal player" alt="" srcset=""></div>
       <div class="music__dsc">
         <div class="name">${song.name}</div>
         <div class="author">${song.author}</div>
@@ -27,10 +42,10 @@ function createSongElement(song) {
     <div class="music__add right">
       <div class="time">${song.time}</div>
       <div class="dwn" style="cursor:pointer;">
-        <svg fill="#757f8f" width="28px" height="28px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+        <svg fill="#757f8ff" width="28px" height="28px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M256,409.7,152.05,305.75,173.5,284.3l67.33,67.32V34h30.34V351.62L338.5,284.3,360,305.75ZM445.92,351v93.22a3.61,3.61,0,0,1-3.47,3.48H69.15a3.3,3.3,0,0,1-3.07-3.48V351H35.74v93.22A33.66,33.66,0,0,0,69.15,478h373.3a33.85,33.85,0,0,0,33.81-33.82V351Z"
-            stroke="#757f8f"
+            stroke="#757f8ff"
             stroke-width="45"
             stroke-linejoin="round"
           />
@@ -190,4 +205,3 @@ document.addEventListener('keydown', (e) => {
 
 // Загрузка песен при старте
 loadSongs();
-
